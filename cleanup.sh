fc023a11fabc02206a5abdd56cab8559e3076b20
David <d-hari@hotmail.com>
2025-11-29 21:10:50 +1100

    Increased number of files deleted per execution, and added logging.

--- cleanup.sh
+++ cleanup.sh
@@ -8,13 +8,14 @@ min_free=$((4 * 1024 * 1024))
 free_space=$(df "$video_dir" | awk 'NR==2 {print $4}')
 
 # Max number to delete, to prevent infinite loop or script taking too long
-max_count=20
+max_count=50
 count=0
 
 # Delete oldest files until free space is above threshold
 while [ "$free_space" -lt "$min_free" ] && [ "$count" -lt "$max_count" ]; do
 	oldest_file=$(find "$video_dir" -type f -printf '%T+ %p\n' | sort | head -n 1 | cut -d' ' -f2-)
 	[ -z "$oldest_file" ] && exit 0
+	echo "Deleting $oldest_file"
 	rm -f "$oldest_file"
 	free_space=$(df "$video_dir" | awk 'NR==2 {print $4}')
 	((count++))
